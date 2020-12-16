184ab653c5913b3cd8d8fce9e65c64ede81a3e69
Murat ERDEM <muraterdemcs@gmail.com>
2020-12-16 12:30:47 +0300

    Create T1016.sh

---
+++ mitre/T1016/T1016.sh
@@ -0,0 +1,4 @@
+if [ -x "$(command -v arp)" ]; then arp -a; else echo "arp is missing from the machine. skipping..."; fi;
+if [ -x "$(command -v ifconfig)" ]; then ifconfig; else echo "ifconfig is missing from the machine. skipping..."; fi;
+if [ -x "$(command -v ip)" ]; then ip addr; else echo "ip is missing from the machine. skipping..."; fi;
+if [ -x "$(command -v netstat)" ]; then netstat -ant | awk '{print $NF}' | grep -v '[a-z]' | sort | uniq -c; else echo "netstat is missing from the machine. skipping..."; fi;
