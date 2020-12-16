35922b7ee3a5878caf2e48f3913358dd821b8bdd
Murat ERDEM <muraterdemcs@gmail.com>
2020-12-16 15:48:10 +0300

    Update T1027.sh

--- mitre/T1027/T1027.sh
+++ mitre/T1027/T1027.sh
@@ -1,4 +1,4 @@
-sh -c "echo ZWNobyAiTWFsd2F0aW9uIHdhcyBoZXJlIg== > /tmp/encoded.dat"
+echo "ZWNobyAiTWFsd2F0aW9uIHdhcyBoZXJlIg==" > /tmp/encoded.dat
 cat /tmp/encoded.dat | base64 -d > /tmp/art.sh
 chmod +x /tmp/art.sh
 /tmp/art.sh
