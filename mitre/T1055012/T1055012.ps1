ec50ca8f47865b6f17b9d26a3a3e8e909ecacc65
olcsy <59606576+olcsy@users.noreply.github.com>
2021-03-31 13:52:05 +0300

    Update T1055012.ps1

--- mitre/T1055012/T1055012.ps1
+++ mitre/T1055012/T1055012.ps1
@@ -1,3 +1,3 @@
-.$PathToAtomicsFolder\Start-Hollow.ps1
+.$env:TEMP\Start-Hollow.ps1
 $ppid=Get-Process "explorer.exe"
 Start-Hollow -Sponsor "C:\Windows\System32\calc.exe" -Hollow "C:\Windows\System32\cmd.exe" -ParentPID $ppid -Verbose
